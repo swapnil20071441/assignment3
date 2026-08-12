b23bf0599079b4355e897954414e5cbd272b1da8
swapnil20071441 <swapnilbhoite9810@gmail.com>
2026-08-12 10:11:41 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+#bubble sort
 list=[5,2,4,3,1]
 n=len(list)
 
@@ -5,4 +6,17 @@ for i in range(n):
     for j in range(0,n-i-1):
         if list[j]>list[j+1]:
             list[j],list[j+1]=list[j+1],list[j]
-print("list is sorted",list)
+print("bubble sort",list)
+
+
+#selection sort
+for i in range(n):
+    min_index = i
+
+    for j in range(i + 1, n):
+        if list[j] < list[min_index]:
+            min_index = j
+
+    list[i], list[min_index] = list[min_index], list[i]
+
+print("Selection Sort:", list)
